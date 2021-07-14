393512358fbc94e26e78b2fd4e4ea0a0d14ab546
yoonsery <yunsel2@gmail.com>
2021-07-14 22:10:13 +0900

    initialize

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+const add = document.querySelector('.add');
+const minus = document.querySelector('.minus');
+const number = document.querySelector('span');
+let count = 0;
+number.textContent = count;
 
+const updateText = () => {
+  number.textContent = count;
+};
+
+const handleAdd = () => {
+  count = count + 1;
+  updateText();
+};
+
+const handleMinus = () => {
+  count = count - 1;
+  updateText();
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
