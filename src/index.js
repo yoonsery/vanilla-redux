d996c71a8a9d9e7fe2e6ea8d39659ae85e9ffe8a
yoonsery <yunsel2@gmail.com>
2021-07-15 01:12:34 +0900

    modify to switch

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ const number = document.querySelector('span');
 
 number.textContent = 0;
 
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
 const countModifier = (count = 0, action) => {
-  if (action.type === 'ADD') {
-    return (count = count + 1); // reducer가 리턴하는 것이 어플리케이션의 state가 된다
-  } else if (action.type === 'MINUS') {
-    return (count = count - 1);
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -22,13 +26,7 @@ const onChange = () => {
   number.textContent = countStore.getState();
 };
 
-// change를 store에서 감지하고 싶으면 subscribe하면 된다
 countStore.subscribe(onChange);
 
-console.log(countStore.getState());
-
-// dispatch가 reducer를 불러서 action을 보낸다
-// (action은 reducer와 커뮤니케이트하는 방법)
-// action은 오브젝트이고 type이 있어야 함
-add.addEventListener('click', () => countStore.dispatch({ type: 'ADD' }));
-minus.addEventListener('click', () => countStore.dispatch({ type: 'MINUS' }));
+add.addEventListener('click', () => countStore.dispatch({ type: ADD }));
+minus.addEventListener('click', () => countStore.dispatch({ type: MINUS }));
