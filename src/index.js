2d9967858e876bf570a2a3933184471afe8a759f
yoonsery <yunsel2@gmail.com>
2021-07-14 23:49:05 +0900

    learn subscribe, getstate

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const add = document.querySelector('.add');
 const minus = document.querySelector('.minus');
 const number = document.querySelector('span');
 
+number.textContent = 0;
+
 const countModifier = (count = 0, action) => {
   if (action.type === 'ADD') {
     return (count = count + 1);
@@ -16,11 +18,13 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'MINUS' });
+const onChange = () => {
+  number.textContent = countStore.getState();
+};
+
+countStore.subscribe(onChange);
 
 console.log(countStore.getState());
+
+add.addEventListener('click', () => countStore.dispatch({ type: 'ADD' }));
+minus.addEventListener('click', () => countStore.dispatch({ type: 'MINUS' }));
