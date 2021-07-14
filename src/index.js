8c82e5cc165f264cb53400adeaa361cff265eef3
yoonsery <yunsel2@gmail.com>
2021-07-15 01:04:33 +0900

    explain

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ number.textContent = 0;
 
 const countModifier = (count = 0, action) => {
   if (action.type === 'ADD') {
-    return (count = count + 1);
+    return (count = count + 1); // reducer가 리턴하는 것이 어플리케이션의 state가 된다
   } else if (action.type === 'MINUS') {
     return (count = count - 1);
   } else {
@@ -22,9 +22,13 @@ const onChange = () => {
   number.textContent = countStore.getState();
 };
 
+// change를 store에서 감지하고 싶으면 subscribe하면 된다
 countStore.subscribe(onChange);
 
 console.log(countStore.getState());
 
+// dispatch가 reducer를 불러서 action을 보낸다
+// (action은 reducer와 커뮤니케이트하는 방법)
+// action은 오브젝트이고 type이 있어야 함
 add.addEventListener('click', () => countStore.dispatch({ type: 'ADD' }));
 minus.addEventListener('click', () => countStore.dispatch({ type: 'MINUS' }));
