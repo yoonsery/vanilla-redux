9908783c8e874ae503e09241638da31626ca9904
yoonsery <yunsel2@gmail.com>
2021-07-14 23:37:02 +0900

    add action

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ const add = document.querySelector('.add');
 const minus = document.querySelector('.minus');
 const number = document.querySelector('span');
 
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  if (action.type === 'ADD') {
+    return (count = count + 1);
+  } else if (action.type === 'MINUS') {
+    return (count = count - 1);
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
 
 console.log(countStore.getState());
