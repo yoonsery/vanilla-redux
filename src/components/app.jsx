09b759870210c6aa17f0cbc744c6673762dbaa55
yoonsery <yunsel2@gmail.com>
2021-07-15 17:38:33 +0900

    set up for redux-react

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Detail from '../routes/detail';
+import Home from '../routes/home';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/:id" component={Detail}></Route>
+      </Router>
+    </>
+  );
+}
+
+export default App;
